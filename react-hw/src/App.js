207f48bc971071356119456e9679e18c9dd9fca4
Stanislau Karbanovich <karbanovichstanislav@gmail.com>
2025-06-20 15:36:54 +0200

    feat: add 39.2 px

--- react-hw/src/App.js
+++ react-hw/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { Button } from './components/Button';
+import { Card } from './components/Card';
 
 function App() {
   return (
     <div>
       <Button buttonText="Find Out More"/>
+      <div style={{display:"flex", gap:"20px"}}>
+        <Card price="15.000 ₽" school="УМК по робототетхнике "/ >
+        <Card price="15.000 ₽" school="УМК по программированию "/ >
+      </div>
     </div>
   );
 }
---
+++ react-hw/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import "./Card.scss";
+
+export const Card = (props) => {
+    return (
+        <div className="card">
+            <h3 className="card_title">– ROBO –</h3>
+            <p className="card_price">{props.price}</p>
+            <p className="card_school">{props.school}</p>
+            <button className="card_button"></button>
+        </div>
+    )
+}
